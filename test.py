31cf6513a91be8338eb45344bd0a6044cbfc98ff
Vijay Gupta <vjgupta57@gmail.com>
2018-11-01 20:27:49 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,53 @@
+import numpy as np
+import cv2
+import  imutils
+import sys
+import pytesseract
+
+
+image = cv2.imread('car.jpeg')
+
+image = imutils.resize(image, width=500)
+
+cv2.imshow("Original Image", image)
+
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+cv2.imshow("1 - Grayscale Conversion", gray)
+
+gray = cv2.bilateralFilter(gray, 11, 17, 17)
+cv2.imshow("2 - Bilateral Filter", gray)
+
+edged = cv2.Canny(gray, 170, 200)
+cv2.imshow("4 - Canny Edges", edged)
+
+(new, cnts, _) = cv2.findContours(edged.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+cnts=sorted(cnts, key = cv2.contourArea, reverse = True)[:30] 
+NumberPlateCnt = None 
+
+count = 0
+for c in cnts:
+        peri = cv2.arcLength(c, True)
+        approx = cv2.approxPolyDP(c, 0.02 * peri, True)
+        if len(approx) == 4:  
+            NumberPlateCnt = approx 
+            break
+
+# Masking the part other than the number plate
+mask = np.zeros(gray.shape,np.uint8)
+new_image = cv2.drawContours(mask,[NumberPlateCnt],0,255,-1)
+new_image = cv2.bitwise_and(image,image,mask=mask)
+cv2.namedWindow("Final_image",cv2.WINDOW_NORMAL)
+cv2.imshow("Final_image",new_image)
+
+config = ('-l eng --oem 1 --psm 3')
+
+# Read image from disk
+#im = cv2.imread(new_image, cv2.IMREAD_COLOR)
+
+# Run tesseract OCR on image
+text = pytesseract.image_to_string(new_image, config=config)
+
+# Print recognized text
+print(text)
+
+cv2.waitKey(0)
